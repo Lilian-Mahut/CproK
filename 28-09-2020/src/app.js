25e58251f90baad32e92f812cc0d64e7c592abf8
Lilian Mahut <lilian.mahut@pm.me>
2020-09-29 09:42:59 +0200

    finish!

--- 28-09-2020/src/app.js
+++ 28-09-2020/src/app.js
@@ -1,12 +1,24 @@
-const boxes = document.querySelector('box')
+// const boxes = document.querySelector('box')
+// const observer = new IntersectionObserver(callback)
+
+// function callback(entries) {
+//     entries.forEach(entry => {
+//         console.log(entry)
+//         if (entry.isIntersecting)
+//         entry.classList.add('visible')
+//     })
+// }
+
+// boxes.forEach(box => observer.observe(box))
+
+const boxes = document.querySelectorAll(".box")
+
 const observer = new IntersectionObserver(callback)
 
 function callback(entries) {
-    entries.forEach(entry => {
-        console.log(entry)
-        if (entry.isIntersecting)
-        entry.classList.add('visible')
-    })
+	entries.forEach(entry => {
+		if (entry.isIntersecting) entry.target.classList.add("visible")
+	})
 }
 
 boxes.forEach(box => observer.observe(box))
